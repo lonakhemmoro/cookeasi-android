f497e8012bcd686c3278500d5e237898150c83d6
bennytalks <bennytoeppe@gmail.com>
2023-04-11 11:36:48 -0400

    commented out code, added more ingredients (they need to be ID'd properly)

--- app/src/main/java/com/cookeasibenny/AddFridgeActivity.java
+++ app/src/main/java/com/cookeasibenny/AddFridgeActivity.java
@@ -15,34 +15,40 @@ public class AddFridgeActivity extends AppCompatActivity {
 
     //each button is broken down into respective catagories
     //imagebuttons that set visibility for each LL section
+    //Cbasics is the navigation buttons in the middle of the activity
 private ImageButton CBasics, CGrains, CVegetables, CProtein, CFruit;
-//basics table
+//basics table, added more so add the proper buttons here
 private Button Butter, Chili,egg,pepper,cilantro,flour,honey,chiliF,salt,cumin,pbutter,soysauce,sugar,vinagrette,broth,oregano,basil,oliveoil;
-//grains table
+//grains table, added more so add the proper buttons here
 private Button Toast, wrap,spaghetti,granola,quinoa;
-
+//veggies table
 private Button lime,cucumber,avocado,onion,carrot,veggiemix,celery,driedLentils,tomato,cannedtomato,bellpepper,blackbeans,potato,ginger,garlic,lettuce;
-
+//protein table
 private Button yogurt,cheese,chicken,almondmilk,hummus,groundbeef;
-
+//fruit table
 private Button frzbanana,frzberries;
-
+//lowest buttons
 private Button backtoFridge, BackToMenu;
+//view groups for the collapsing tables
 private android.view.ViewGroup BasicsVG, GrainsVG,VegetablesVG,ProteinVG,FruitVG;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //dont mess w this line, connects the xml to the java
         setContentView(R.layout.activity_add_fridge);
-
+//these are linking the image buttons to the variables
         CBasics = (ImageButton) findViewById(com.cookeasibenny.R.id.BasicsCollapsebtn);
         CGrains = (ImageButton) findViewById(com.cookeasibenny.R.id.GrainsCollapsebtn);
         CVegetables = (ImageButton) findViewById(com.cookeasibenny.R.id.VegetablesCollapseBtn);
         CProtein = (ImageButton) findViewById(com.cookeasibenny.R.id.ProteinCollapseBtn);
         CFruit = (ImageButton) findViewById(com.cookeasibenny.R.id.FruitCollapseBtn);
-
+// connects the viewgroups to the layouts, all layouts that are relavent are labeled in the xml
         BasicsVG = findViewById(com.cookeasibenny.R.id.BasicsCollapse);
         BasicsVG.setVisibility(android.view.View.GONE);
+        //^ do this for all view groups pls
+
+        // onclick listeners for nav bar
         CBasics.setOnClickListener( new View.OnClickListener(){
             public void onClick(View x){VisibilityBasics();}
         });
@@ -55,8 +61,13 @@ private android.view.ViewGroup BasicsVG, GrainsVG,VegetablesVG,ProteinVG,FruitVG
             public void onClick(View x){VisibilityProtein();}});
         CFruit.setOnClickListener( new View.OnClickListener(){
             public void onClick(View x){VisibilityFruit();}});
-    }
 
+        //please add onclick listeners for all buttons for food here
+        //add food buttons above.
+    }
+//public voids for visiiblity of food groups for buttons
+    //all but visibilityBasics are empty, but that is the example, change R.id.BasicsCollapse to
+    //respective collapse layout
     private void VisibilityGrains() {
 
     }
