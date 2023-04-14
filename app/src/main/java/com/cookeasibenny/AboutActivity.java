397c8188a1255653980ab0ac75de81b5d7c7629f
Bedhead11 <prongerchase@gmail.com>
2023-04-14 18:16:29 -0400

    About Button Menu Return

--- app/src/main/java/com/cookeasibenny/AboutActivity.java
+++ app/src/main/java/com/cookeasibenny/AboutActivity.java
@@ -1,14 +1,28 @@
 package com.cookeasibenny;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class AboutActivity extends AppCompatActivity {
 
+    private Button BackMenu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        Button backButton = findViewById(R.id.BackMain_about);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {openBackMenu();}
+        });
+    }
+    public void openBackMenu(){
+        Intent openBackMenuIntent = new Intent(this, MainActivity.class);
+        startActivity(openBackMenuIntent);
     }
 }
