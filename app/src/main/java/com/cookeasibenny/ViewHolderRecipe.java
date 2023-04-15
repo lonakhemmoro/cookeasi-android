404cde8279865dd78ab553058d47136bc8142465
joeybrozowski1 <jbrozowski@oakland.edu>
2023-04-15 16:03:22 -0400

    Revert "Merge branch 'main' of https://github.com/lonakhemmoro/cookeasi-android"
    This reverts commit e86f274826d35ea31f217bcfb06ace2f6c796904, reversing
    changes made to 12d2e56f2a5395ccfa64710049919e16437cb4d8.

--- app/src/main/java/com/cookeasibenny/ViewHolderRecipe.java
+++ app/src/main/java/com/cookeasibenny/ViewHolderRecipe.java
@@ -2,36 +2,22 @@
 package com.cookeasibenny;
 
 import androidx.recyclerview.widget.RecyclerView;
-import androidx.appcompat.app.AppCompatActivity;
+
 import android.widget.ImageView;
 import android.widget.TextView;
-import  android.widget.Button;
-import android.os.Bundle;
-import android.content.Intent;
+
 public class ViewHolderRecipe extends RecyclerView.ViewHolder {
 
     ImageView imageView1;
     TextView textView1;
     TextView textView2;
 
-    Button MakeItBtn1;
-
     public ViewHolderRecipe(android.view.View itemView) {
         super(itemView);
         imageView1 = itemView.findViewById(com.cookeasibenny.R.id.imageView1);
         textView1 = itemView.findViewById(com.cookeasibenny.R.id.Title);
         textView2 = itemView.findViewById(com.cookeasibenny.R.id.subheaderReicpe);
-        MakeItBtn1 = itemView.findViewById(com.cookeasibenny.R.id.MakeItBtn);
-MakeItBtn1.setOnClickListener(new android.view.View.OnClickListener() {
-    @Override
-    public void onClick(android.view.View v) {
-        ClickIntent();
-    }
-});
-    }
 
-    public void ClickIntent(){
 
     }
-
 }
