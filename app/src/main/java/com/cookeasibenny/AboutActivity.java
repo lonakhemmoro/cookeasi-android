faa67a89323124a3d09df0ce1802f4d8d39c4feb
bennytalks <bennytoeppe@gmail.com>
2023-04-20 21:52:50 -0400

    added github link

--- app/src/main/java/com/cookeasibenny/AboutActivity.java
+++ app/src/main/java/com/cookeasibenny/AboutActivity.java
@@ -20,7 +20,28 @@ public class AboutActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {openBackMenu();}
         });
+        Button button = findViewById(com.cookeasibenny.R.id.GithubButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://github.com/lonakhemmoro/cookeasi-android";
+                Intent intent = new Intent(Intent.ACTION_VIEW, android.net.Uri.parse(url));
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.setPackage("com.android.chrome");
+                try {
+                    startActivity(intent);
+                } catch (android.content.ActivityNotFoundException e) {
+                    intent.setPackage(null);
+                    startActivity(intent);
+                }
+            }
+        });
+
+
+
     }
+
+
     public void openBackMenu(){
         Intent openBackMenuIntent = new Intent(this, MainActivity.class);
         startActivity(openBackMenuIntent);
